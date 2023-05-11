6e5947034785ebc20960f123ad83060c467e3994
sadboimk <118701236+sadboimk@users.noreply.github.com>
2023-05-11 10:26:58 +0800

    new project

---
+++ lesson13.js
@@ -0,0 +1,50 @@
+const song1 = new Audio(`audio/ if looks could kill.mp3`)
+
+function playSongOne() {
+    console.log(`playing song 1...`)
+    song1.play()
+}
+
+function pauseSongOne() {
+    console.log(`pausing song 1...`)
+    song1.pause()
+}
+
+function loadSongOne() {
+    console.log(`Loading song 1...`)
+    song1.load()
+}
+
+const song2 = new Audio(`im sad .mp3`)
+
+function playSongTwo() {
+    console.log(`playing song 2...`)
+    song2.play()
+}
+
+function pauseSongTwo() {
+    console.log(`pausing song 2...`)
+    song2.pause()
+}
+
+function loadSongTwo() {
+    console.log(`Loading song 2...`)
+    song2.load()
+}
+
+const song3 = new Audio(`Infrared.mp3`)
+
+function playSongThree() {
+    console.log(`playing song 3...`)
+    song3.play()
+}
+
+function pauseSongThree() {
+    console.log(`pausing song 3...`)
+    song3.pause()
+}
+
+function loadSongThree() {
+    console.log(`Loading song 3...`)
+    song3.load()
+}
